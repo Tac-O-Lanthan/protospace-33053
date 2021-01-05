fc07eb96fc426f82a92f65bccb8fed7487e59de8
Tac-O-Lanthan <75772499+Tac-O-Lanthan@users.noreply.github.com>
2021-01-05 19:33:36 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# README
+
+# テーブル設計
+
+## users テーブル
+
+| Columna      | Type   | Options     |
+| ------------ | ------ | ----------- |
+| email        | string | null: false |
+| password     | string | null: false |
+| name         | string | null: false |
+| profile      | text   | null: false |
+| occupation   | text   | null: false |
+| position     | text   | null: false |
+
+### Association
+
+- has_many :prototypes
+- has_many :comments
+
+## prototypes テーブル
+
+| Column     | Type       | Options     |
+| ---------- | ---------- | ----------- |
+| title      | string     | null: false |
+| catch_copy | text       | null: false |
+| concept    | text       | null: false |
+| image      |            |             |
+| user       | references |             |
+
+### Association
+
+- belongs_to :users
+- has_many :comments
+
+## comments テーブル
+
+| Column    | Type       | Options                        |
+| --------- | ---------- | ------------------------------ |
+| text      | text       | null: false, foreign_key: true |
+| user      | references |                                |
+| protorype | references |                                |
+
+### Association
+
+- belongs_to :room
+- belongs_to :user
---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,63 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  # prototypeディレクトリのビューはindexとshow「以外」ログイン必須
+  before_action :move_to_root, except: [:index, :show]
+  # 直接URLを打ち込んでアクセスしても、current_userと一致しなければリダイレクトする
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+  # @prototype = Prototype.find(params[:id]) をまとめて読み込み
+
+  def index
+    @prototypes = Prototype.includes(:user)
+    # @prototypes = Prototype.all だとN+1問題で負担がかかる処理になる
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    # createで保存すると以下if~endの処理を挟めないので、new~saveに分解する
+    if @prototype.save
+      redirect_to prototype_path(@prototype.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments
+  end
+
+  def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_root
+    redirect_to root_path unless current_user == @prototype.user
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+end
